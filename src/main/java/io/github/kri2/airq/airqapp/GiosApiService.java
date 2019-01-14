02f7b2ec5c9b8a5dff86566f78c6d89b5b30d8f4
Krzysztof Turlej <christur@interia.pl>
2019-01-14 13:28:17 +0100

    added loop to try 5 times if readout is null

--- src/main/java/io/github/kri2/airq/airqapp/GiosApiService.java
+++ src/main/java/io/github/kri2/airq/airqapp/GiosApiService.java
@@ -15,11 +15,25 @@ public class GiosApiService
     
         ParameterItem parameterPm25 = restTemplate.getForObject("http://api.gios.gov.pl/pjp-api/rest/data/getData/2772", ParameterItem.class);
         //System.out.println(parameterPm25.toString());
-        map.put(parameterPm25.getKey(),parameterPm25.getValues().get(1).getValue()); // TODO: for now second, but later to be determind which one is valid and freshest
+        // index 0 in List is most current readout, but often its value is nulll, if that's the case read next (up to 5 readouts prior)
+        int indexToRead=0;
+        while(parameterPm25.getValues().get(indexToRead).getValue()==null && indexToRead<5)
+        {
+            indexToRead++;
+        }
+        map.put(parameterPm25.getKey(),parameterPm25.getValues().get(indexToRead).getValue());
+        
         
         ParameterItem parameterPm10 = restTemplate.getForObject("http://api.gios.gov.pl/pjp-api/rest/data/getData/2770", ParameterItem.class);
         //System.out.println(parameterPm10.toString());
-        map.put(parameterPm10.getKey(),parameterPm10.getValues().get(1).getValue()); // TODO: for now second, but later to be determind which one is valid and freshest
+        // index 0 in List is most current readout, but often its value is nulll, if that's the case read next (up to 5 readouts prior)
+        indexToRead=0;
+        while(parameterPm10.getValues().get(indexToRead).getValue()==null && indexToRead<5)
+        {
+            indexToRead++;
+        }
+        
+        map.put(parameterPm10.getKey(),parameterPm10.getValues().get(indexToRead).getValue()); // TODO: for now second, but later to be determind which one is valid and freshest
         
         return map;
     }
